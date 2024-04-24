19307541b15ad3fa22ce5ff72ef396a163ed29ed
regularwave <regularwave@gmail.com>
2024-04-23 20:22:36 -0400

    removed nonfunctional counter

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const visCount = document.getElementById('visCount');
-updateSiteCounter();
-
 var museumItems;
 
 function itemGen() {
@@ -81,12 +78,4 @@ function genLocationsList() {
         span.appendChild(document.createTextNode(element));
         locations.appendChild(document.createElement('br'));
     })
-}
-
-function updateSiteCounter() {
-    fetch('https://api.countapi.xyz/hit/regularwave_SV-Donation-Location-Finder')
-        .then(response => response.json())
-        .then(response => {
-            visCount.innerHTML = response.value;
-        })
 }
